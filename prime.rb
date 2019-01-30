8a393d0992b68be82b68bb1f12d8881992b9e3be
Tony Baker <this.tonybaker@gmail.com>
2019-01-30 04:20:54 +0000

    Done.

--- prime.rb
+++ prime.rb
@@ -1,1 +1,14 @@
-# Add  code here!
+def prime?(number)
+  if number == 2
+    return true
+  elsif number <= 1
+    return false
+  end
+  range_array = (2..number - 1).to_a
+  range_array.each do |x|
+    if number % x == 0
+      return false
+    end
+  end
+  return true
+end
